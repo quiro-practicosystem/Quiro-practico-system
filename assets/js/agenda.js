96ddf2ada78116a68a8223236c1317280ceae801
quiro-practicosystem <quiropractico1998system@gmail.com>
2026-01-27 01:01:38 -0600

    Update agenda.js

--- assets/js/agenda.js
+++ assets/js/agenda.js
@@ -3,17 +3,43 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const calendar = new FullCalendar.Calendar(calendarEl, {
     locale: 'es',
-    initialView: 'timeGridWeek',
+    initialView: window.innerWidth < 768 ? 'timeGridDay' : 'timeGridWeek',
     height: 'auto',
+    nowIndicator: true,
+    expandRows: true,
+
     headerToolbar: {
       left: 'prev,next today',
       center: 'title',
       right: 'dayGridMonth,timeGridWeek,timeGridDay'
     },
+
+    buttonText: {
+      today: 'Hoy',
+      month: 'Mes',
+      week: 'Semana',
+      day: 'Día'
+    },
+
     selectable: true,
+    selectMirror: true,
+
     dateClick(info) {
-      alert('Agendar cita el ' + info.dateStr);
-    }
+      alert(`Nueva cita: ${info.dateStr}`);
+    },
+
+    events: [
+      {
+        title: 'Consulta',
+        start: '2026-01-27T10:00:00',
+        end: '2026-01-27T11:00:00'
+      },
+      {
+        title: 'Ajuste',
+        start: '2026-01-27T12:00:00',
+        end: '2026-01-27T12:30:00'
+      }
+    ]
   });
 
   calendar.render();
